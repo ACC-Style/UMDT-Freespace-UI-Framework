f184dd7eb587805f05878b336dd9fecb6f558215
Aquib Sylvester <aquib.designs@gmail.com>
2018-10-24 13:57:31 -0400

    UI - card forms

---
+++ app/scripts/controls/micro.interaction.card.js
@@ -0,0 +1,17 @@
+// use strict;
+// code draft:
+// grab all items with micro data attribute
+// add to array collect
+// function to determine attribute value
+// functions for each value type
+
+var $ = require('jquery');
+
+
+console.groupCollapsed(`// Micro-Interactions: cards`);
+
+
+
+console.groupEnd();
+
+init();
